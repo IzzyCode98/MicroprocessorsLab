71a47b10bef4868da789a46d176b2fd770dfb307
isabe <isabe@DESKTOP-886E80I>
2020-10-22 12:25:44 +0100

    Changed output port to PORTC instead of PORTB. Changed loop condition to end at decimal value 9 instead of 99.
    Set PORTD to input port. Change loop condition to be the input value at PORTD.

--- main.s
+++ main.s
@@ -4,21 +4,23 @@
 psect	code, abs
 	
 main:
-	org	0x0
+	org	0x0	
 	goto	start
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
+	movlw	0xff
+	movwf	TRISD, A	    ; set tristate D value to be all 1s therefore input pin
+	movff	PORTD, 0x30	    ; move the value input at port D to address 0x30
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
 	movff 	0x06, PORTC
-	incf 	0x06, W, A
+	incf 	0x06, F, A
 test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x09	    ; Decimal number 9
-	cpfsgt 	0x06, A
+	movf	0x30 		    ; move value in address 0x30 to W
+	cpfsgt 	0x06, A		    ; compare PORTC value to PORTD value
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
