534f87a3dedc06cca033d98e24c4fae63a8a850a
isabe <isabe@DESKTOP-886E80I>
2020-10-26 16:27:36 +0400

    Changed output port to PORTC instead of PORTB. Changed loop condition to end at decimal value 9 instead of 99.
    Set PORTD to input port. Change loop condition to be the input value at PORTD.
    
    Cleaned up issues with code. Successfully ran on microprocessor with PORTD input on for RD7 switch.

--- main.s
+++ main.s
@@ -10,16 +10,18 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw	0xff
-	movwf	TRISD, A	    ; set tristate D value to be all 1s therefore input pin
+	movwf	TRISD, A	    ; set tristate D value to be all 1's therefore input pin
 	movff	PORTD, 0x30	    ; move the value input at port D to address 0x30
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
+	clrf	LATC, A		    ; clears the output
 	bra 	test
 loop:
 	movff 	0x06, PORTC
-	incf 	0x06, F, A
+	incf 	0x06, W, A
 test:
-	movf	0x30 		    ; move value in address 0x30 to W
+	movwf	0x06, A		    ; move W value into address 0x06
+	movf	0x30, W, A	    ; move value in address 0x30 to W
 	cpfsgt 	0x06, A		    ; compare PORTC value to PORTD value
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
